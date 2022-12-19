dd6aae185c9a5f833f2bf497af4eeebfa5334eab
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-19 08:29:25 +0300

    First ViewModel Test

--- app/src/test/java/com/viewmodel/ArtViewModelTest.kt
+++ app/src/test/java/com/viewmodel/ArtViewModelTest.kt
@@ -3,16 +3,22 @@ package com.viewmodel
 import com.FakeArtRepository
 import com.volkankelleci.artbooktesting.viewmodel.ArtViewModel
 import org.junit.Before
+import org.junit.Test
 
 class ArtViewModelTest {
     private lateinit var viewModel: ArtViewModel
 
     @Before
-    fun setup(){
+    fun setup() {
 
-        viewModel= ArtViewModel(FakeArtRepository())
+        viewModel = ArtViewModel(FakeArtRepository())
 
     }
 
+    @Test
+        //makeart fun tests
+    fun withoutName(){
+        viewModel.makeArt("","YES","BUZ")
 
+    }
 }
