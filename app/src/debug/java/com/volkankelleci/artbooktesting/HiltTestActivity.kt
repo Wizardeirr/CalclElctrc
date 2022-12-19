58d323af4fd169bb9fead001323df9a839a8f3a3
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-19 15:05:02 +0300

    Hilt Dagger Tests started

---
+++ app/src/debug/java/com/volkankelleci/artbooktesting/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.volkankelleci.artbooktesting
+
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.appcompat.app.AppCompatActivity
+
+@AndroidEntryPoint
+class HiltTestActivity:AppCompatActivity() {
+}
