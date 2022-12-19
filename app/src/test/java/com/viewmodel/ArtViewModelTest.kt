5c2f0d2b1d678cd8bb094cd58b03727ebe02b352
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-19 10:03:17 +0300

    First Test passed on VM

--- app/src/test/java/com/viewmodel/ArtViewModelTest.kt
+++ app/src/test/java/com/viewmodel/ArtViewModelTest.kt
@@ -1,12 +1,26 @@
 package com.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.FakeArtRepository
+import com.google.common.truth.Truth.assertThat
+import com.volkankelleci.artbooktesting.MainCoroutineRule
+import com.volkankelleci.artbooktesting.util.Status
 import com.volkankelleci.artbooktesting.viewmodel.ArtViewModel
+import com.volkankelleci.getOrAwaitValueTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class ArtViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule=InstantTaskExecutorRule()
+    @get:Rule
+    var mainCoRoutineRule=MainCoroutineRule()
     private lateinit var viewModel: ArtViewModel
+
 
     @Before
     fun setup() {
@@ -19,6 +33,8 @@ class ArtViewModelTest {
         //makeart fun tests
     fun withoutName(){
         viewModel.makeArt("","YES","BUZ")
+        val value=viewModel.insertArtMessage.getOrAwaitValueTest()
+        assertThat(value.status).isEqualTo(Status.ERROR)
 
     }
 }
