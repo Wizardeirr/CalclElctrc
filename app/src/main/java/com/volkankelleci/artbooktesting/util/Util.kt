bf582e75b408161ad22b17f5f0da0607b2066f3b
[kellecivolkan@gmail.com] <kellecivolkan@gmail.com>
2023-03-19 21:42:47 +0300

    Util Created

--- app/src/main/java/com/volkankelleci/artbooktesting/util/Util.kt
+++ app/src/main/java/com/volkankelleci/artbooktesting/util/Util.kt
@@ -4,4 +4,25 @@ object Util {
     const val API_KEY="23520173-1a644aa914588dcab8799703f"
     const val BASE_URL="https://pixabay.com/api/"
 
+    fun calculateBill(units: Int): Int {
+
+        if (units <= 100) {
+            return units * 5
+        }
+        else if (units >= 101 && units<=500) {
+            return (100 * 5 + (units - 100) * 8)
+        }
+        else if (units > 500) {
+            return (100 * 5 + 100 * 8 + ((units - 200)* 10))
+        }
+
+        return 0
+    }
+    fun callCalc(units:Int){
+        if (units!=null){
+            calculateBill(units)
+        }
+
+
+    }
 }
